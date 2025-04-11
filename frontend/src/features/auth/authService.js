03e6844ef079a85f673ad3a5fd2126b6e416517b
Melody Cartwright <melody94c@gmail.com>
2025-04-11 11:39:54 +0200

    testing

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
-// Make sure this ends in `/auth`
 const API_URL = `${import.meta.env.VITE_API_BASE_URL}/auth`;
 
 const register = async (userData) => {
-  const res = await axios.post(`${API_URL}/register`, userData);
+  const res = await axios.post(API_URL + "register", userData);
   return res.data;
 };
 
 const login = async (userData) => {
-  const res = await axios.post(`${API_URL}/login`, userData);
+  const res = await axios.post(API_URL + "login", userData);
   return res.data;
 };
 
